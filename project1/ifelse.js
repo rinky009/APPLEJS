70407063014fb1c93e90aead2dd997aeeb3cec46
Shikha Rani <srani009@gmail.com>
2022-02-15 06:29:47 +0530

    ifelsechange

--- project1/ifelse.js
+++ project1/ifelse.js
@@ -1,9 +1,9 @@
 const num=11;
 if(num==11){
-    console.log('number is 11');
+    console.log('number is 11')
 }
 else if(num==12){
-console.log('number is 12');
+console.log('number is 12')
 }
 else{
     console.log('number is not 11');
