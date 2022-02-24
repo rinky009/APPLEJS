fcad17b53f7c5d0892f9de157dddb33eca407a84
Shikha Rani <srani009@gmail.com>
2022-02-25 04:41:36 +0530

    syn&asyn

---
+++ 24.2.22 syncandasync/callback2.js
@@ -0,0 +1,10 @@
+const funA=()=>{
+    setTimeout(function(){
+        console.log('welcome fun1');
+    },3000);
+}
+//const funB=()=>{
+    //console.log('welcome fun2')
+//}
+//funB();
+funA();
---
+++ 24.2.22 syncandasync/syneg1.js
@@ -0,0 +1,5 @@
+//most of the programming languages are synchronous-execution /satatement goes one other.
+const a=6;
+const b=5
+const add=a+b;
+console.log(add);
